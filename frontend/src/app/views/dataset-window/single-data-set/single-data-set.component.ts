61a94d7605abc9f6f120a37a131a4fcde4fa2637
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2023-05-12 18:34:31 +0200

    edit entries for data-set

--- frontend/src/app/views/dataset-window/single-data-set/single-data-set.component.ts
+++ frontend/src/app/views/dataset-window/single-data-set/single-data-set.component.ts
@@ -4,7 +4,7 @@ import {DataSetService} from "../../../services/data-set.service";
 @Component({
   selector: 'app-single-data-set',
   templateUrl: './single-data-set.component.html',
-  styleUrls: ['./single-data-set.component.css']
+  styleUrls: ['./single-data-set.component.scss']
 })
 export class SingleDataSetComponent {
   id: string;
